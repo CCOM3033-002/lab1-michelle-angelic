ef71a63bbe58fe84a16803dc9ce7d5d1e9abb1e2
michelle-angelic <michelle.santiago19@upr.edu>
2025-02-07 19:50:56 +0000

    please save github

--- main.cpp
+++ main.cpp
@@ -7,3 +7,22 @@
 #include <iomanip>
 
 using namespace std;
+
+// ask for the name of the movie
+
+string movie_name;
+cout << "Please enter the name of the movie: ";
+cin >> movie_name;
+
+// ask for how many adult tickets were sold 
+
+double adult_tickets; 
+cout << "Please enter how many adult tickets were sold: "; 
+cin >> adult_tickets;
+
+// ask how many children's tickets were sold
+
+double children_tickets; 
+cout << "Please enter how many children tickets were sold: ";
+cin >> children_tickets
+
